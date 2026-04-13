3f1dfcdcff5521ebf2337074b5567c2da186d1a1
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2026-04-13 22:24:02 +0530

    server code for chunkserver

--- cmd/chunkserver/main.go
+++ cmd/chunkserver/main.go
@@ -1,1 +1,28 @@
 package chunkserver
+
+import (
+	"github.com/Yakshith15/GFS/internal/chunkserver/storage"
+	"github.com/Yakshith15/GFS/internal/chunkserver/server"
+	"log"
+	"google.golang.org/grpc"
+	"net"
+	"github.com/Yakshith15/GFS/api"
+)
+
+func main() {
+	storage, err := storage.NewStorage("gfs/chunks")
+	if err != nil {
+		log.Fatalf("failed to create storage: %v", err)
+	}
+	server := server.NewServer(storage)
+	grpcServer := grpc.NewServer()
+	api.RegisterChunkServerServiceServer(grpcServer, server)
+	lis, err := net.Listen("tcp", ":5001")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Println("Chunk server running on port 5001...")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
